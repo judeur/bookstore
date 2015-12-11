5c30ba0fcb8c2f07a89dba59cdf6ea335738585a
Judith Deurvorst <judeur@gmail.com>
2015-12-11 22:38:12 +0100

    reset image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,14 +4,14 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
 	#Include the sprockets-rails helper for Rails 4+ asset pipeline compatibility:
   include	Sprockets::Rails::Helper
   
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  storage :file
+  # storage :fog
   
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -42,7 +42,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
-  %w(jpg jpeg gif png pdf mp4 wmv xls xlsx doc docx ppt pptx mov)
+  %w(jpg jpeg gif)
   # end
 
   # Override the filename of the uploaded files:
