90354b4291861913fbab8d892176cc885d08706f
Judith Deurvorst <judeur@gmail.com>
2015-12-13 14:44:14 +0100

    add author to products db

--- db/migrate/20151203203231_create_products.rb
+++ db/migrate/20151203203231_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.string :author
       t.text :description
       t.string :image_url
       t.decimal :price
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209230702) do
+ActiveRecord::Schema.define(version: 20151213130632) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20151209230702) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "author"
   end
 
 end
