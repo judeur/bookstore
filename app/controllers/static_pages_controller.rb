5a1eb824b299da535f147d7c7290ca773859069d
judeur <judeur@gmail.com>
2016-01-20 22:44:31 +0100

    set up Heroku mail, style contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,9 +11,6 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail( :from => @email,
-                              :to => 'judeur@gmail.com',
-                              :subject => "A new contact form message from #{@name}",
-                              :body => @message).deliver_now
+    UserMailer.create_contact_form(@email, @name, @message).deliver_now
   end
 end
