330509f86f38600a8b49f4102f05b9bab768b511
judeur <judeur@gmail.com>
2016-01-27 21:29:45 +0100

    Create comment model. Style show page product.

--- app/assets/javascripts/notice.js
+++ app/assets/javascripts/notice.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $(".alert").fadeIn(700).delay(1500).fadeOut(700);
     $(".alert-danger").fadeIn(700).delay(1500).fadeOut(700);
+    $(".alert-alert").fadeIn(700).delay(1500).fadeOut(700);
     $("#error_explanation").fadeIn(700).delay(3000).fadeOut(700);
 });
