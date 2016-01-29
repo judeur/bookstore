108489b7a84254766ad9f61a08a2e47bd5aad824
judeur <judeur@gmail.com>
2016-01-29 21:59:23 +0100

    Configure permitted parameters for devise. Style footer.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # to permit additional parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
     flash.now[:error] = "Access denied"
     redirect_to root_url
+  end
+
+ protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:destroy) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
   end
 end
