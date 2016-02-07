2a9f0fab2e456b2f67c0ce12a887815f456f02f6
judeur <judeur@gmail.com>
2016-02-07 19:00:37 +0100

    Polish layout navbar top.

--- app/assets/javascripts/notice.js
+++ app/assets/javascripts/notice.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $(".alert").fadeIn(700).delay(2500).fadeOut(700);
     $("#error_explanation").fadeIn(700).delay(3000).fadeOut(700);
+    $('#flash').hide().delay(800).fadeIn(800).delay(4000).fadeOut(800)
 });
