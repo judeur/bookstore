d09d10723cccd4d08a6508188dc1a26c4195086a
judeur <judeur@gmail.com>
2016-02-07 21:26:42 +0100

    Add rspec.

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.2.3'
 
 group :development, :test do
 	gem 'sqlite3'
+  gem 'rspec-rails', '~> 3.0'
 end
 
 gem "carrierwave"
--- test/mailers/previews/user_mailer_preview.rb
+++
@@ -1,6 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailer/user_mailer
-class UserMailerPreview < ActionMailer::Preview
-  def contact_form
-    UserMailer.contact_form("john@example.com", "John", "Hello World!")
-  end
-end
