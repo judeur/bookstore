3ad1dabbfdd39b53768960763a1c46a705290ce7
judeur <judeur@gmail.com>
2016-04-12 21:17:27 +0200

    Create page with Angular to add and remove orders.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,26 +1,39 @@
 class OrdersController < ApplicationController
-  protect_from_forgery with: :null_session
-  # before_action :logged_in_user
-  before_filter :authenticate_user!
+  protect_from_forgery
+  skip_before_action :verify_authenticity_token, if: :json_request?
+  respond_to :json, :html
 
 	def index
+		@orders = Order.all.to_json(:include => [{:product => {:only => :name}},
+    {:user => {:only => :email}}])
+    respond_with @orders
 	end
 	
 	def show
+    @order = Order.find(params[:id]).to_json(:include => [{:product => {:only => :name}},
+    {:user => {:only => :email}}])
+    respond_with @order
 	end
 	
 	def new
 	end
 	
 	def create
+    @order = Order.create(order_params)
+    respond_with @order
 	end
 	
 	def destroy
-	end
+    respond_with Order.destroy(params[:id])
+  end
+  
+  protected
+    def json_request?
+      request.format.json?
+    end
 
   private
-    # def logged_in_user
-      # unless logged_in?
-      # flash[:danger] = "Please log in to view your orders."
-      # redirect_to login_url
+    def order_params
+      params.require(:order).permit(:product_id, :user_id, :total)
+    end
 end
